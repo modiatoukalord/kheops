14edf383f57b80331c28c497e3bb10ed1b6d8680
Lord Modiatouka <lmodiatouka@gmail.com>
2025-08-20 20:44:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/admin/booking-schedule.tsx
+++ src/components/admin/booking-schedule.tsx
@@ -23,6 +23,7 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
 import { cn } from "@/lib/utils";
+import { FormControl } from "@/components/ui/form";
 
 
 const initialBookings = [
