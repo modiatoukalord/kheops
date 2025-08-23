21275ce2c57091bb3ab8f584ab513c7b7eb91aa6
Lord Modiatouka <lmodiatouka@gmail.com>
2025-08-23 22:05:15 +0000

    { apiKey: "AIzaSyBszUJS1QdD3c2BVnH5HQZ9a-16NVHOBR4", authDomain: "kheops

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +6,12 @@ import { initializeApp, getApps } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBszUJS1QdD3c2BVnH5HQZ9a-16NVHOBR4",
-  authDomain: "kheops-hub.firebaseapp.com",
-  projectId: "kheops-hub",
-  storageBucket: "kheops-hub.firebasestorage.app",
-  messagingSenderId: "444707922937",
-  appId: "1:444707922937:web:9a1f03c413e0f8b1995328"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
