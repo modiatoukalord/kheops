fa0c9c09016909e7fe1dac740af10d2c501783ec
Lord Modiatouka <lmodiatouka@gmail.com>
2025-09-27 16:04:59 +0000

    sur version mobile place aussi un bouton pour fermer (_for element img_)

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -6,6 +6,7 @@ import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
+import { Button } from "./button"
 
 const Dialog = DialogPrimitive.Root
 
@@ -45,10 +46,13 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground sm:block hidden">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
+      <DialogClose asChild className="sm:hidden">
+          <Button variant="outline" className="mt-4">Fermer</Button>
+      </DialogClose>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
@@ -121,5 +125,3 @@ export {
   DialogTitle,
   DialogDescription,
 }
-
-    
